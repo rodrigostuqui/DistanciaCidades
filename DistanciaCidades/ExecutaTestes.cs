7421ca9d14931656f00503ce414132ebbc3b1fce
rodrigostuqui <rst.monzani@gmail.com>
2022-12-19 14:19:16 -0300

    Feat: "Adicionando Leitura do arquivo"

--- DistanciaCidades/ExecutaTestes.cs
+++ DistanciaCidades/ExecutaTestes.cs
@@ -12,13 +12,6 @@ namespace Question1
         {
             int caminho;
             Distancia distancia = new Distancia();
-            Console.WriteLine("Digite o caminho feito, para parar, digite -1");
-            int.TryParse(Console.ReadLine(), out caminho);
-            while (caminho != -1)
-            {
-                distancia.adicionaCaminho(caminho);
-                int.TryParse(Console.ReadLine(), out caminho);
-            }
             Console.WriteLine(distancia.distanciaCaminho());
         }
     }
--- DistanciaCidades/Program.cs
+++ DistanciaCidades/Program.cs
@@ -1,27 +1,50 @@
 ﻿
 namespace Question1
 {
-    public partial class Distancia
+    public class Distancia
     {
         private List<int> Caminho { get; set; } = new List<int>();
+
+        private List<List<int>> Cidades { get; set; } = new List<List<int>>();
+
+        public Distancia()
+        {
+            StreamReader sr = new StreamReader($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\matrix.txt");
+            string line = sr.ReadLine();
+            while ( line != null )
+            {
+                Cidades.Add(Utils.StringParaLista(line));
+                line = sr.ReadLine();
+            }
+            sr = new StreamReader($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\caminho.txt");
+            line = sr.ReadLine();
+            this.Caminho = Utils.StringParaLista(line);
+
+        }
         public int distanciaCaminho()
         {
             int total = 0;
             for (int i = 0; i < this.Caminho.Count - 1; i++)
             {
-                total = total + cidades[this.Caminho[i] - 1, this.Caminho[i + 1] - 1];
+                total = total + Cidades[this.Caminho[i] - 1][this.Caminho[i + 1] - 1];
             }
             return total;
         }
 
-        public void adicionaCaminho(int cidade)
+    }
+
+    public class Utils
+    {
+        public static List<int> StringParaLista(string str)
         {
-            if (cidade > 5 || cidade < 1)
+            List<int> num = new List<int>();
+            int numero;
+            foreach (var s in str.Split(','))
             {
-                Console.WriteLine("Cidade Inexistente");
-                return;
+                if(int.TryParse(s, out numero))
+                { num.Add(numero); }
             }
-            Caminho.Add(cidade);
+            return num;
         }
     }
 }
