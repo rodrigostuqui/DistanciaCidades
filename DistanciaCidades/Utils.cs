126de609a625e371453b3bacf10ce8a23865a983
rodrigostuqui <rst.monzani@gmail.com>
2022-12-21 14:10:55 -0300

    feat: Adicionando utils

---
+++ DistanciaCidades/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanciaCidades.Utils
+{
+    public class Utils
+    {
+        public static List<int> StringParaLista(string str) => str.Split(',').Select(int.Parse).ToList();
+    }
+}
